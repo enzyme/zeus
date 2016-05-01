c0c108d599b44598c07d43dc11a2d21f44f11f34
Tristan Strathearn <r3oath@gmail.com>
2016-05-01 14:05:46 +1000

    Improve build system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var stripCssComments = require('gulp-strip-css-comments');
 
 gulp.task('build-dev', function() {
     return gulp
@@ -7,3 +10,14 @@ gulp.task('build-dev', function() {
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./bin'));
 });
+
+gulp.task('build-prod', ['build-dev'], function() {
+    return gulp
+        .src('./bin/zeus.css')
+        .pipe(stripCssComments({ preserve: false }))
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./bin'));
+});
+
+gulp.task('build', ['build-dev', 'build-prod']);
