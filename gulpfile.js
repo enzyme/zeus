16fcfce64bb68028a9ca3f228cd63affc8a9db5c
Tristan Strathearn <tstrathearn@bcm.com.au>
2017-03-08 17:30:35 +1000

    Sets up branch with base v9 system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin');
 var rename = require("gulp-rename");
-var stripCssComments = require('gulp-strip-css-comments');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
 
-var processors = [
-    autoprefixer({ browsers: ['last 2 versions'] }),
-];
-
-gulp.task('build-dev', function() {
+gulp.task('build', function() {
     return gulp
-        .src('./src/zeus-dev.scss')
+        .src('./test.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(rename('zeus.css'))
-        .pipe(postcss(processors))
         .pipe(gulp.dest('./bin'));
 });
-
-gulp.task('build-prod', ['build-dev'], function() {
-    return gulp
-        .src('./bin/zeus.css')
-        .pipe(stripCssComments({ preserve: false }))
-        .pipe(postcss(processors))
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./bin'));
-});
-
-gulp.task('build', ['build-dev', 'build-prod']);
 
 gulp.task('default', ['build']);
